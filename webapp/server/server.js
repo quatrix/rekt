bafa68ac19b0813906f8c96086f3195cd91ec602
quatrix <quatrix@Admins-MacBook-Pro.local>
2016-03-06 13:01:11 +0200

    make gulp run

--- webapp/server/server.js
+++ webapp/server/server.js
@@ -1,25 +1,10 @@
-var express = require('express'),
-    registration = require('./lib/registration');
-
-
-
+var express = require('express');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
-});
-
-
-app.post('api/v1/registration/mail', function(req, res){
-    console.log(req.body);
-    res.json({sent: true});
-});
-
-app.post('api/v1/registration/confirm', function(req, res){
-    console.log(req.body);
-    res.json({sent: true});
 });
 
 app.post('api/v1/save', function(req, res){
